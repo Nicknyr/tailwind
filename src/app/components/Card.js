042690e8ecb0243a818ef354be6a00a9b5f4f491
Nick Kinlen <nick_kinlen@hotmail.com>
2025-04-10 17:43:42 -0400

    Added checkmarks, fixed button, adjusted padding

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,14 +1,18 @@
 import "tailwindcss";
-
+import Image from "next/image";
+import Checkmark from '../../../public/checkmark.svg';
 
 const Card = () => {
     return (
         <div className="
             h-auto 
-            w-110 border-1 
-            border-grey-100 
+            w-110 border 
+            border-gray-300
             rounded-3xl 
-            p-12
+            p-8
+            shadow-xl
+            text-grey-50
+            font-light
             ">
              <h2 className="
                 text-xl 
@@ -23,11 +27,23 @@ const Card = () => {
                 <span className="p-1">/month</span>
             </p>
             <p>The perfect plan if you're just getting started with our product.</p>
-            <ul className="mt-10 mb-10">
-                <li className="pb-2">25 products</li>
-                <li className="pb-2">Up to 10,000 subscribers</li>
-                <li className="pb-2">Advanced analytics</li>
-                <li className="pb-2">24-hour support response time</li>
+            <ul className="mt-7 mb-7">
+                <li className="pb-2 flex">
+                    <Image src={Checkmark} height="15" width="15"/> 
+                    <p className="pl-3">25 products</p>
+                </li>
+                <li className="pb-2 flex">
+                    <Image src={Checkmark} height="15" width="15"/> 
+                    <p className="pl-3">Up to 10,000 subscribers</p>
+                </li>
+                <li className="pb-2 flex">
+                    <Image src={Checkmark} height="15" width="15"/> 
+                    <p className="pl-3">Advanced analytics</p>
+                </li>
+                <li className="pb-2 flex">
+                    <Image src={Checkmark} height="15" width="15"/> 
+                    <p className="pl-3">4-hour support response time</p>2
+                </li>
             </ul>
             <button className="h-auto w-auto bg-white border-1 p-3 rounded-md w-full cursor-pointer font-medium text-blue-700 border-blue-400 hover:border-blue-700">Get started today</button>
         </div>    
